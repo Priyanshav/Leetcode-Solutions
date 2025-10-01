1342b39a1643c2459a5b2f80352066f341af1237
Priyanshav <priyanshav@kali.hacker>
2025-10-01 17:54:51 +0530

    bst

---
+++ 108.java
@@ -0,0 +1,20 @@
+public class 108 {
+    class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length == 0) return null;
+
+        return getBST(nums, 0, nums.length-1);
+    }
+
+    public TreeNode getBST(int[] nums, int startIdx, int endIdx){
+        if(startIdx > endIdx) return null;
+
+        int middleIdx = (startIdx + endIdx)/2;
+        TreeNode bst = new TreeNode(nums[middleIdx]);
+        bst.left = getBST(nums, startIdx, middleIdx-1);
+        bst.right = getBST(nums, middleIdx+1, endIdx);
+
+        return bst;
+    }
+}
+}
