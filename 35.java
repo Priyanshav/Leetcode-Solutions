bab49e376d2f10ea9347a4f60009ac2d3b74ed96
Priyanshav <priyanshav@kali.hacker>
2025-10-01 17:35:12 +0530

    array

---
+++ 35.java
@@ -0,0 +1,21 @@
+public class 35 {
+    class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int result = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target){ 
+                result = i;
+                break;
+            }
+            else if(nums[i] > target){
+                result = i;
+                break;
+            }
+            else{
+                result = nums.length;
+            }
+        }
+        return result;
+    }
+}
+}
