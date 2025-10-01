c0800779fdabd55d223d24c3ccdfe04dd9ea3160
Priyanshav <priyanshav@kali.hacker>
2025-10-01 18:18:31 +0530

    two pointers

---
+++ 350.java
@@ -0,0 +1,30 @@
+public class 350 {
+    class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] finalResult = new int[result.size()];
+        for(int k = 0; k < finalResult.length; k++){
+            finalResult[k] = result.get(k);
+        } 
+        return finalResult;
+    }
+}
+}
