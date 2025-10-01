b22f1e0994cdbd6b66e74ada8f37d9110e1da260
Priyanshav <priyanshav@kali.hacker>
2025-10-01 18:14:05 +0530

    one pointer

---
+++ 283.java
@@ -0,0 +1,17 @@
+public class 283 {
+    class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroPointer = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[nonZeroPointer];
+                nums[nonZeroPointer] = nums[i];
+                nums[i] = temp;
+                nonZeroPointer++;
+            }
+        }
+        System.out.println(nums);
+    }
+}
+}
